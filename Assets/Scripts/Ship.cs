337e5ae9d2f9a4b3b273157062cb668408b7d118
Farfellelo <marmerthomas@gmail.com>
2015-04-25 15:19:59 -0400

    Ship movement

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Ship : Damagable {
 
+    private const float FORWARD_FORCE = 75;
+    private const float BACKWARD_FORCE = 50;
+    private const float ROTATION = 100;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,25 @@ public class Ship : Damagable {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector2 forward = new Vector2(-Mathf.Sin(transform.localEulerAngles.z*Mathf.PI/180),Mathf.Cos(transform.localEulerAngles.z*Mathf.PI/180));
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(forward * FORWARD_FORCE * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce(-forward * BACKWARD_FORCE * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, ROTATION * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -ROTATION * Time.deltaTime);
+        }
 	}
 }
