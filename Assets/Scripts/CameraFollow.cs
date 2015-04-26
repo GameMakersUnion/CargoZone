6ca79224e55ad952d04467e266a5fb1e1b665f50
Victor <victor.nguyen@hotmail.com>
2015-04-25 21:28:54 -0400

    Camera Following
    OffsetX = 6 (Worked Well)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+    public float offsetX = 0.0f;
+    public float offsetY = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(target != null)
+        {
+            transform.position = new Vector3(target.position.x+ offsetX, target.position.y+ offsetY, transform.position.z);
+        }
+	}
+}
