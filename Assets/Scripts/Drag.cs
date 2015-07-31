639f9f907fbe61cf54a930cbec4b7c8a47d21e5c
Farfellelo <marmerthomas@gmail.com>
2015-07-30 21:10:33 -0400

    Clicky stuff

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -6,6 +6,14 @@ public class Drag : MonoBehaviour
 {
     private Vector3 screenPoint;
     private Vector3 offset;
+    Rigidbody2D rigid;
+    float decelleration;
+
+    void Start()
+    {
+        decelleration = 1;
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
     void OnMouseDown()
     {
@@ -15,8 +23,24 @@ public class Drag : MonoBehaviour
 
     void OnMouseDrag()
     {
+
+        //get pos
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+        float speed = Mathf.Sqrt(Mathf.Pow(curPosition.x - transform.position.x, 2) + Mathf.Pow(curPosition.y - transform.position.y, 2));
+
+        //not in hold area
+        if (gameObject.layer != 8)
+        {
+            //make follow mouse directly
+            rigid.velocity = (curPosition - transform.position).normalized * speed / Time.deltaTime;
+
+        }
+        else
+        {
+            decelleration /= 2f;
+            rigid.velocity = (curPosition - transform.position).normalized * speed / Time.deltaTime * decelleration;
+            Debug.Log(rigid.velocity); 
+        }
     }
 }
