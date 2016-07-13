03bdaaaa1c2b4b3dea9305f3ce6b34d3593731e8
Victor <victor.nguyen@hotmail.com>
2016-07-13 14:36:39 -0400

    Draging works, but breaks out of ship

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -8,22 +8,34 @@ public class Drag : MonoBehaviour
     private Vector3 offset;
     Rigidbody2D rigid;
     float decelleration;
+    private SpringJoint2D springJoint;//Used to pull rididbody
 
     void Start()
     {
         decelleration = 1;
         rigid = GetComponent<Rigidbody2D>();
+        springJoint = gameObject.AddComponent<SpringJoint2D>() as SpringJoint2D;
+        springJoint.enabled = false;
+        springJoint.distance = 0.0f;
+        springJoint.dampingRatio = 1.0f;
+        springJoint.frequency = 1.0f;
+        springJoint.autoConfigureDistance = false;
     }
 
     void OnMouseDown()
     {
-
+        Debug.Log("Mouse Down");
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        springJoint.enabled = true;
     }
 
     void OnMouseDrag()
     {
-
+        if (springJoint) {
+            springJoint.connectedAnchor = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+        /*
+        Debug.Log("Mouse Drag");
         //get pos
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
@@ -42,5 +54,12 @@ public class Drag : MonoBehaviour
             rigid.velocity = (curPosition - transform.position).normalized * speed / Time.deltaTime * decelleration;
             Debug.Log(rigid.velocity); 
         }
+        */
+
+    }
+
+    void OnMouseUp()
+    {
+        springJoint.enabled = false;
     }
 }
